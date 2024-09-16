e1f9e60c292a676bc36277a4969bdfdd4c088b95
chhtrisamir7 <samirchhtri24@gmail.com>
2024-09-16 03:15:11 +0300

    change path of package.json file in start

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,18 @@
 const express = require('express')
 const dotenv = require('dotenv').config() // Load environment variables
-const connectDB = require('./config/db')
+const connectDB = require('./config/db') 
 
 // Connect Data Base
 connectDB()
 
 const app = express()
 
-
+const PORT = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Server is Ready....');
 })
-
-const PORT = process.env.PORT;
-// connect 
+ 
 
 app.listen(PORT, () => {
     console.log(`Server is running http://localhost:${PORT}`)
